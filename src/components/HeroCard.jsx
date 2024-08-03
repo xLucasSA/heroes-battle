503b3f5a056debdedaa747d04f528efd73373771
Lucas <lsdeabre@gmail.com>
2024-08-03 19:09:05 -0300

    Styling cards and hero list

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,15 +1,65 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
-const HeroCard = ({key: heroId, hero}) => {
+const HeroCard = ({ hero }) => {
+  let [imageUrl, setImageUrl] = useState(hero.images.xs);
+  const [selected, setSelected] = useState(false);
+
+  const handleCardClick = () => {
+    setSelected(!selected);
+    onSelectHero(hero.id); // Envia o ID do herói para o componente pai
+  };
+
+  const updateImageUrl = () => {
+    const width = window.innerWidth;
+    if (width < 768) {
+      setImageUrl(hero.images.sm);
+    } else if (width < 1024) {
+      setImageUrl(hero.images.md);
+    } else {
+      setImageUrl(hero.images.lg);
+    }
+  };
+
+  useEffect(() => {
+    updateImageUrl();
+    window.addEventListener('resize', updateImageUrl);
+    
+    return () => {
+      window.removeEventListener('resize', updateImageUrl);
+    };
+  }, [hero.images]);
+
   return (
-    <div key={heroId} className="hero">
-      {hero.name}
-      {hero.powerstats}
+    <div 
+      className={`max-w-xs rounded overflow-hidden shadow-lg border-4 bg-white hover:bg-blue-200 transition-all p-7 flex flex-col items-center last:justify-end cursor-pointer ${selected ? 'border-blue-500 bg-blue-200' : ''}`}
+      onClick={handleCardClick}
+    >
+      <img className="sm:h-52 md:h-60 lg:h-72 object-cover rounded" src={imageUrl} alt={hero.name} />
+      <div className="w-full py-4 h-48">
+        <div className="font-bold text-xl mb-2 text-black text-center">{hero.name}</div>
+        <p className="text-gray-700 text-base text-left">
+          {hero.biography.fullName}
+        </p>
+        <p className="text-gray-600 text-sm mb-2 text-left">
+          {hero.biography.placeOfBirth}
+        </p>
+      </div>
+      <div>
+        {Object.entries(hero.powerstats).map(([stat, value]) => (
+          <span
+            key={stat}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >
+          {stat.charAt(0).toUpperCase() + stat.slice(1)}: {value}
+          </span>
+        ))}
+      </div>
     </div>
   );
-};
-
-export default HeroCard;
+  };
+  
+  export default HeroCard;
 
 /*
 {
--- src/components/HeroList.jsx
+++ src/components/HeroList.jsx
@@ -3,7 +3,7 @@ import HeroCard from './HeroCard';
 
 const HeroList = ({ heroes }) => {
   return (
-    <div className="hero-list">
+    <div className="px-2 py-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
       {heroes.map(hero => (
         <HeroCard key={hero.id} hero={hero} />
       ))}
