fd2288903e327aabcb36583e1dc81addb711fa1d
Lucas <lsdeabre@gmail.com>
2024-08-03 20:39:05 -0300

    Create pagination for heroes displayed

--- src/components/HeroList.jsx
+++ src/components/HeroList.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import HeroCard from './HeroCard';
+import { useState, useEffect } from 'react';
 
 const HeroList = ({ heroes, selectedHeroIds, onSelectHero }) => {
+  const [displayedHeroes, setDisplayedHeroes] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(20);
+
+  useEffect(() => {
+    setDisplayedHeroes(heroes.slice(0, visibleCount));
+  }, [heroes, visibleCount]);
+
+  const loadMoreHeroes = () => {
+    if (visibleCount < heroes.length) {
+      setVisibleCount(prevCount => prevCount + 20);
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+        loadMoreHeroes();
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [visibleCount, heroes.length]);
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-      {heroes.map(hero => (
+    <div className="pt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+      {displayedHeroes.map(hero => (
         <HeroCard 
           key={hero.id} 
           hero={hero} 
