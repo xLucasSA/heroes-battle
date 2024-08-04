19d41ed2c98b1066e6ce4f7fd020f14363540f2b
Lucas <lsdeabre@gmail.com>
2024-08-03 23:22:20 -0300

    Remove console.log

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,6 @@ const API_URL = '/api/ps/metahumans';
 export const fetchHeroes = async () => {
   try {
     const response = await axios.get(API_URL);
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error('Error fetching heroes:', error);
